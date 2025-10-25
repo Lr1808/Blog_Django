879e2136b16533d5a5a34af1b49dcc5ef8104a87
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-25 11:10:30 -0700

    Blog agregado

---
+++ blogs/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import publication 
+
+# Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'updated_date')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('published_date', 'updated_date', 'author')
+
+admin.site.register(publication, PublicationAdmin)
+
---
+++ blogs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class publication(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+                                'auth.User',
+                                on_delete=models.CASCADE
+                              )
+    
+    def __str__(self):
+        return self.title
