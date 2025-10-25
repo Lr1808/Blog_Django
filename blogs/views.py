4ea0b1981273795a1465c8d4e42de3a38331000c
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-25 12:19:26 -0700

    template agregado

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import publication
+from django.views.generic import ListView
 # Create your views here.
+
+
+class PublicationListView(ListView):
+    model = publication
+    template_name = 'home.html'
+
+    
